c25ce6869c45ed9329b7c14a9241bd689d95f657
Paolo Soncco <paholo2054@gmail.com>
2023-08-12 20:45:09 -0500

    sqlalchemy and create user model

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
-from users import users_list
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Text, Boolean
 
 app = Flask(__name__)
+db = SQLAlchemy()
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@localhost:3306/flask_repaso"
+
+db.init_app(app)
+
+class UserModel(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(Text, nullable=False)
+    status = Column(Boolean, default=True)
 
 @app.route('/')
 def hello_world():
-    return 'Bienvenido a mi API! 😎'
-
-@app.route('/users')
-def users():
-    return users_list
+    return 'Hello, World! 😎'
 
 if __name__ == '__main__':
     app.run(debug=True)
